b763d2074a0a68187240862c82b0b0947b593688
Jayson Alihassan Diaz <95516878+jaysonprincediaz@users.noreply.github.com>
2022-05-27 20:30:55 +0800

    Pricing feature

--- src/pages/home/Pricing/index.styles.tsx
+++ src/pages/home/Pricing/index.styles.tsx
@@ -1,7 +1,7 @@
 /* eslint-disable import/prefer-default-export */
 import styled from 'styled-components';
 
-import BG1 from 'src/assets/BG-1.jpg';
+import BG2 from 'src/assets/BG-2.jpg';
 
 export const HeaderOne = styled.h1`
   font-size: 100px;
@@ -23,7 +23,7 @@ export const Paragraph = styled.p`
 export const WeGotYouContainer = styled.div`
   display: flex;
   flex-direction: column;
-  background-image: url(${BG1});
+  background-image: url(${BG2});
   height: 100vh;
   justify-content: flex-start;
   color: white;
@@ -38,10 +38,10 @@ export const CardsContainer = styled.div`
 `;
 
 export const CardContainer = styled.div`
-  border-radius: 2px;
+  border-radius: 8px;
   width: 20rem;
   height: 25rem;
-  background-color: #303030;
+  background-color: #009c9f;
   cursor: pointer;
   transform: translateZ(0);
   transition: transform 0.25s ease-out;
@@ -53,17 +53,17 @@ export const CardContainer = styled.div`
   }
 `;
 
-export const CardImage = styled.div`
-  height: 60%;
-`;
-
-export const Image = styled.img`
-  width: 100%;
-  height: 100%;
+export const CardIcon = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  font-size: 150px;
+  height: 40%;
+  margin-top: 2rem;
 `;
 
 export const TextContent = styled.div`
-  height: 40%;
+  height: 60%;
   width: 100%;
   padding: 25px;
   display: flex;
@@ -72,11 +72,30 @@ export const TextContent = styled.div`
 `;
 
 export const HeaderTwo = styled.h2`
-  margin: auto;
+  font-family: 'Oswald', sans-serif;
+  font-size: 35px;
 `;
 
 export const PTag = styled.p`
   display: flex;
   justify-content: center;
   margin-left: 20px;
+  top: 10rem;
+`;
+
+export const ButtonPrice = styled.button`
+  background: #2c2c2c;
+  border-radius: 3px;
+  border: 2px solid transparent;
+  color: #ffffff;
+  margin-top: 1rem;
+  width: 100%;
+  height: 25%;
+  font-size: 25px;
+  cursor: pointer;
+
+  &:hover {
+    background-color: white;
+    color: #009c9f;
+  }
 `;
--- src/pages/home/Pricing/index.tsx
+++ src/pages/home/Pricing/index.tsx
@@ -1,9 +1,68 @@
+/* eslint-disable react/button-has-type */
+/* eslint-disable object-curly-newline */
 import React from 'react';
+
+import { IoMdRibbon } from 'react-icons/io';
+
+import {
+  WeGotYouContainer,
+  HeaderOne,
+  Paragraph,
+  CardsContainer,
+  CardIcon,
+  TextContent,
+  CardContainer,
+  HeaderTwo,
+  PTag,
+  ButtonPrice,
+} from './index.styles';
 
 const Pricing = (): React.ReactElement => {
   return (
     <div>
-      <h1>Pricing</h1>
+      <WeGotYouContainer>
+        <HeaderOne>PRICING</HeaderOne>
+        <Paragraph>
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmodtempor incididunt ut labore et dolore
+          magna aliqua. Ut enim ad minim veniam,quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
+          commodoconsequat.
+        </Paragraph>
+        <CardsContainer>
+          <CardContainer>
+            <CardIcon>
+              <IoMdRibbon />
+            </CardIcon>
+
+            <TextContent>
+              <HeaderTwo>BASIC</HeaderTwo>
+              <PTag>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</PTag>
+              <ButtonPrice>$150.00</ButtonPrice>
+            </TextContent>
+          </CardContainer>
+          <CardContainer>
+            <CardIcon>
+              <IoMdRibbon />
+            </CardIcon>
+
+            <TextContent>
+              <HeaderTwo>PROFESSIONAL</HeaderTwo>
+              <PTag>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</PTag>
+              <ButtonPrice>$200.00</ButtonPrice>
+            </TextContent>
+          </CardContainer>
+          <CardContainer>
+            <CardIcon>
+              <IoMdRibbon />
+            </CardIcon>
+
+            <TextContent>
+              <HeaderTwo>ULTIMATE</HeaderTwo>
+              <PTag>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</PTag>
+              <ButtonPrice>$250.00</ButtonPrice>
+            </TextContent>
+          </CardContainer>
+        </CardsContainer>
+      </WeGotYouContainer>
     </div>
   );
 };
